f59ba5b650c020c6da5de8cd2eff3b37ff6d4521
Abdur-Rahman Islam <abdelrahmanislam116@gmail.com>
2026-01-18 11:46:00 +0200

    Added views and paths

--- webapp/views.py
+++ webapp/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home_view(request):
+    return render(request, 'home/home.html')
+
+def login_view(request):
+    return render(request, 'registration/login.html')
+
+def stocks_view(request):
+    return render(request, 'home/stocks.html')
+
+def logout_view(request):
+    return render(request, 'registration/logout.html')
+
+def register_view(request):
+    return render(request, 'registration/register.html')
