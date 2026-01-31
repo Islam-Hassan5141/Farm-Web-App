51c41377080581a897884c323d60a46315a2821c
Abdur-Rahman Islam <abdelrahmanislam116@gmail.com>
2026-01-31 23:01:13 +0200

    Turned off Debug Mode

--- farmwebapp/settings.py
+++ farmwebapp/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-#8zz^8f$ar%1+^p6b7o94ja7$3pn0-%@n&y_355$smanw9ywsk'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '192.168.0.119']
 
